d89e64c029791eb90f191c626fb0f52d411dbdfc
Jason Marshall <jmarshall@bemarketingsolutions.com>
2024-08-23 16:43:49 -0400

    initial layout and component creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import Button from "./Components/Button";
+
 function App() {
   return (
     <>
-      <h1 className="text3xl font-bold underline">Business Card</h1>
+      <div className="container flex flex-col md:flex-row bg-white p-10 rounded-lg">
+        <div className="flex-1">
+          <h1 className="text3xl font-bold underline text-black">
+            Business Card
+          </h1>
+        </div>
+        <div className="flex flex-col justify-start content-start flex-1 gap-2">
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laudantium
+            ipsam, atque debitis, ullam eaque qui cumque, aliquid natus quos
+            ipsum mollitia omnis voluptatum saepe. Ad.
+          </p>
+          <Button />
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/Components/Button.tsx
@@ -0,0 +1,9 @@
+function Button() {
+  return (
+    <>
+      <button>My Button</button>
+    </>
+  );
+}
+
+export default Button;
